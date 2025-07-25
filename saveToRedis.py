146ee2729939c2e45cb7bc17ffe0fc85cb57a139
junhyeonpark <j0303p@gmail.com>
2025-07-25 13:55:48 +0900

    fix: 레디스 초기화 시간 추가

--- saveToRedis.py
+++ saveToRedis.py
@@ -5,5 +5,5 @@ import json
 def save_similarity_to_redis(similarity_map,r):
 
     # Redis 저장
-    r.set("question_emotion_similarity.json", json.dumps(similarity_map, ensure_ascii=False, indent=2))
+    r.set("question_emotion_similarity.json", json.dumps(similarity_map, ensure_ascii=False, indent=2), ex=36000)
     print("✅ Redis에 저장 완료!")
