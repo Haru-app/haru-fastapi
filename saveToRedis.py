ce12e12f5850ba0b47a930cf55fcbecad6b83dd9
junhyeonpark <j0303p@gmail.com>
2025-07-28 17:51:09 +0900

    deploy

--- saveToRedis.py
+++ saveToRedis.py
@@ -8,3 +8,4 @@ def save_similarity_to_redis(similarity_map,r):
     r.set("question_emotion_similarity.json", json.dumps(similarity_map, ensure_ascii=False, indent=2), ex=36000)
     print("✅ Redis에 저장 완료!")
 
+
