b2a1d0b1f5657b63f8bdf62c7f181890e07b9b83
jhy <65846779+jhy11@users.noreply.github.com>
2025-07-28 17:33:13 +0900

    deploy

--- saveToRedis.py
+++ saveToRedis.py
@@ -7,3 +7,6 @@ def save_similarity_to_redis(similarity_map,r):
     # Redis 저장
     r.set("question_emotion_similarity.json", json.dumps(similarity_map, ensure_ascii=False, indent=2), ex=36000)
     print("✅ Redis에 저장 완료!")
+
+
+##dfadsfa
